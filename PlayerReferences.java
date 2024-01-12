25e822caf620b9b0ddd56712d728b41a73548de2
Bodhana Sivagurunathan <63922812+bodhanas@users.noreply.github.com>
2024-01-12 10:24:17 -0500

    Add files via upload

---
+++ PlayerReferences.java
@@ -0,0 +1,13 @@
+/**
+ * @author Bodhana S.
+ * @date June 16, 2022
+ * Declared all instances in one class for better organization and passing between methods (Advice from Dad)
+ */
+package sivagurunathanbattleship;
+
+public class PlayerReferences {
+    public static Player1FleetLocationGrid player1inst = null;
+    public static Player2FleetLocationGrid player2inst = null;
+    public static Player1HitMissGrid player1HitMissinst = null;
+    public static Player2HitMissGrid player2HitMissinst = null;
+}
